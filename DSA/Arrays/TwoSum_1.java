f71dbb1f90e66f1f86cf7e979fb586a8dc582654
Geeta Mirashi <geeta@gmail.com>
2026-04-20 12:31:40 +0530

    # Two Sum - Java Solution
    ## Problem Statement
    
    Given an array of integers `nums` and an integer `target`, return the indices of two numbers such that their sum equals the target.
    
    ## Approach Used
    
    Used **Brute Force Approach** with nested loops.
    
    * First loop selects one element.
    * Second loop checks remaining elements.
    * If sum equals target, return indices.
    
    ## Example
    
    Input:
    nums = [2,7,11,15]
    target = 9
    
    Output:
    [0,1]
    
    ## Java Concepts Used
    
    * Arrays
    * Nested Loops
    * Methods
    * Objects
    * Return Arrays
    * Main Method
    
    ## DSA Concepts Used
    
    * Array Traversal
    * Pair Checking
    * Brute Force Search
    
    ## Time Complexity
    
    O(n²)
    
    ## Space Complexity
    
    O(1)
    
    ## What I Learned
    
    * How to solve array problems using loops
    * How to return arrays in Java
    * Dry run and logic building
    * Basic problem-solving approach for interviews
    
    ## Future Optimization
    
    Can be improved using **HashMap** approach with O(n) time complexity.

---
+++ DSA/Arrays/TwoSum_1.java
@@ -0,0 +1,29 @@
+public class TwoSum_1 {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+
+        TwoSum_1 obj = new TwoSum_1();
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = obj.twoSum(nums, target);
+
+        System.out.println("Indexes: " + result[0] + " " + result[1]);
+    }
+}
